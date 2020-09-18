290d7bed8ff264e791f42d6bf85ab1053555f1fd
austinwilson1224 <austin1224@gmail.com>
2020-09-18 16:07:09 -0700

    hw

--- class-notes/9-18.R
+++ class-notes/9-18.R
@@ -35,8 +35,32 @@ letters[r]
 d2  = air[order(air[,"DAILY_AQI_VALUE"]),]
 plot(d2[,"DAILY_AQI_VALUE"],type="l")
 
+air[,"Date"] = as.Date(air[,"Date"],format="")
+data[,"test"] = data[,"Date"] >= as.Date("2020-07-01") & data[,"Date"] <= as.Date("2020-07-31")
+
+
+
+temp = data[,c("Date","Daily.Mean.PM2.5.Concentration")]
+
+dim(temp)[1]
+
+dim(temp)[1] - 100
+dim(temp)[1]
+temp2 = temp[dim(temp)[1]:dim(temp)[1]-100,]
+dim(temp2)
+(dim(temp)[1]-100):dim(temp)[1]
 
 
 
 
+
+plot_pm2.5 = function(d, n = 100)
+{
+  d = d[order(d$Date, decreasing = FALSE),] # to ensure data is sorted
+  n_most_recent = tail(x = d, n = n)
+  n_most_recent2 = d[dim(d)[1]-100:dim(d),]
+  print(n_most_recent2)
+  
+  plot(Daily.Mean.PM2.5.Concentration ~ Date, data = n_most_recent, type="l")
+}
 

d5914a77f8e9e6c29d92d911254a502d8c75c1e0
austinwilson1224 <austin1224@gmail.com>
2020-10-30 12:30:52 -0700

    midterm stuff

--- midterm/practice.R
+++ midterm/practice.R
@@ -19,3 +19,25 @@ class(list)
 test = data.frame(x = c(1,2,3,4,5))
 test$trash = c("austin","john","wilson","is","cool")
 test
+
+
+table(mtcars[, c("cyl", "gear")])
+
+table(mtcars[(mtcars$cyl == 6) & (mtcars$gear %in% c(3,4)),])
+
+
+find1 = function(x){
+  x = 1
+}
+
+test = find1(1232132)
+test
+
+x = 1:5
+seq_along(x, by = 2)
+
+seq_along(along.with=1:5, by = 2)
+
+seq_along(x)
+
+
